68e73b1b3d421d18aac69e2defb5b7f84902adfe
vic30004 <victor_993@msn.com>
2020-10-03 15:26:01 +0300

    added jwt auth

--- app/app/schema.py
+++ app/app/schema.py
@@ -1,8 +1,16 @@
 import graphene
 import tracks.schema
+import users.schema
+import graphql_jwt
 
 
-class Query(tracks.schema.Query, graphene.ObjectType):
+class Query(users.schema.Query, tracks.schema.Query, graphene.ObjectType):
     pass
 
-schema=graphene.schema(query=Query)
+
+class Mutation(users.schema.Mutation, tracks.schema.Mutation):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
