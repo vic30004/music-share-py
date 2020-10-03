6c5588c9b19766c085625de3de0130ef559d440f
vic30004 <victor_993@msn.com>
2020-10-03 15:48:54 +0300

    connected users to tracks

--- app/tracks/models.py
+++ app/tracks/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+
 class Track(models.Model):
     """Model for tracks"""
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    posted_by = models.ForeignKey(
+        get_user_model(), null=True, on_delete=models.CASCADE)
