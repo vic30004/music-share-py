3af2d2f13c99d0fb16934dc442c6bab878a1fcad
vic30004 <victor_993@msn.com>
2020-10-05 13:00:28 +0300

    updated admin pannel

--- app/tracks/admin.py
+++ app/tracks/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import Track, Like
 # Register your models here.
+
+
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'posted_by')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title', 'posted_by')
+    list_per_page = 25
+
+
+admin.site.register(Track, TrackAdmin)
+
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'track')
+    list_display_links = ('id',)
+    search_fields = ('id', 'user')
+    list_per_page = 25
+
+
+admin.site.register(Like, LikeAdmin)
